ca2a117fa63986abd2059b5423c6b4143362c882
Sabrina Yue Wang <yue.wang1996@gmail.com>
2018-02-07 21:42:18 +0000

    Fixed cjarge per ion in inaccessible region + started lorentz motion script

---
+++ lorentzionneutralcoll.py
@@ -0,0 +1,19 @@
+##This file is mainly for looking at B field effects of crystals that are already formed.
+import numpy
+import scipy
+import matplotlib.pyplot as plt
+import math
+from matplotlib import animation
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.animation
+import pandas as pd
+import mpl_toolkits.mplot3d.axes3d as p3
+import itertools
+import time
+from scipy import special
+import pickle
+from tqdm import tqdm
+from numpy import isclose
+from scipy import interpolate
+
+
