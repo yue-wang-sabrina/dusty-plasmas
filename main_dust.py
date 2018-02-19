2425bb651f6232647a1797d19f7364f1374fba8d
Sabrina Yue Wang <yue.wang1996@gmail.com>
2018-02-19 21:05:33 +0000

    change name from crystal to dust

--- main_dust.py
+++ main_dust.py
@@ -1,6 +1,6 @@
 # This file is mainly for looking at B field effects of crystals that are already formed.
 
-from analysis_crystal_beffects import BEffectsAnalysis
+from analysis_dust import BEffectsAnalysis
 from utils.utils import generate_particle_equilibrium_positions, prepare_modified_b_field
 
 from IPython import get_ipython
@@ -12,14 +12,14 @@ ipython.magic('autoreload 2')
 
 beffect1 = BEffectsAnalysis()
 beffect1.create_particles(
-    numparticles=10,
+    numparticles=100,
     initpositions=generate_particle_equilibrium_positions()
 )
 beffect1.create_pairs()
 beffect1.interact_and_iterate(
     iterationsB=100,
     init_iterations=100,
-    method='Gibs',
+    method='NoGibs',
     modified_b_field=prepare_modified_b_field()
 )
 beffect1.sort_positions_of_particles()
