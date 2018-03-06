415406934b09b19aae49f3b6daa967eb4feaedc6
Sabrina Yue Wang <yue.wang1996@gmail.com>
2018-03-06 13:07:38 +0000

    add a setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dusty-plasmas',
+    version='0.1',
+    long_description='MSci Project',
+    packages=find_packages(),
+    install_requires=[],
+)
